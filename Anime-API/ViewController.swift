3438c54ddeed8f1480cb090d880a4a6dad14f58f
Krrishnanunni <krishnanunni2004ka@gmail.com>
2025-06-19 10:22:23 +0530

    Initial Commit

---
+++ Anime-API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anime-API
+//
+//  Created by iroid on 19/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
